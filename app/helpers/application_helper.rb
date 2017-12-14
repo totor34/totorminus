973ff030123f75fd0305b513d7160b2722ec61b4
Cecile Varin <varincecile@hotmail.com>
2017-12-14 18:04:30 +0100

    several front improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     params[:search].blank? ? 1 : params[:search][:passenger_number]
   end
 
+  def set_params(key)
+    return params[:search][key] unless params[:search].blank?
+  end
+
   def choose_photo_path(user)
     user.photo? ? user.photo.path : image_url('default_avatar_pic.png')
   end
